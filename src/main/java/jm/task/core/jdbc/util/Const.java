4626c083c00828421fdab5c30ae7d2833af74422
iv_al3x <tripanosomas@ya.ru>
2020-10-12 19:48:44 +0300

    v.1

---
+++ src/main/java/jm/task/core/jdbc/util/Const.java
@@ -0,0 +1,12 @@
+package jm.task.core.jdbc.util;
+
+public class Const  {
+
+    public static final String USER_TABLE = "users";
+
+    public static final String USERS_ID = "id";
+    public static final String USERS_NAME = "name";
+    public static final String USERS_LASTNAME = "lastname";
+    public static final String USERS_AGE = "age";
+
+}
